95a67e51cf1f9db3a45a0a536c34fb56027e6294
eman-m0hammed <ma3920457@gmail.com>
2023-02-28 16:08:46 +0200

    create user views

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>All Users</title>
+    
+    <style>
+
+        .active {
+            background: gray;
+        }
+        table{
+            overflow: hidden;
+            text-align: center;
+        }
+        table form{
+            display: inline-block;
+        }
+        table td {
+            padding: 5px 10px;
+        }
+        table form button{
+            text-decoration: none;
+            border: 1px solid black;
+            border-radius: 5px;
+            background-color: #dfd7fa;
+            padding: 5px 15px;
+            margin: 0 5px;
+        }
+        .alert-success{
+            background-color: #adb3ed;
+            padding: 20px;
+            font-size: 20px;
+            margin-top: 10px;
+            width: 25%;
+            border-radius: 5px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <a href="{{route('post.create')}}" style="font-size:20px; display:inline-block; margin:20px 5px 5px;">create new post</a>
+    @if(Session::has('success'))
+    <div class="alert-success">{{Session::get('success')}}</div>
+    @endif
+    <h1>All Posts</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th></th>
+                <th>Created at</th>
+                <th>Action</th>
+                
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr class="@if ($loop->first) active @endif">
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at }}</td>
+                    <td>
+                    <form action="{{ route('post.show', $user->id) }}" method="get"><button>Show</button></form>
+                    <form action="{{ route('post.update', $user->id) }}" method="get"> <button>Edit</button></form>
+                    <form action="{{ route('post.destroy', $user->id) }}" method="post"> 
+                        @method('delete')
+                        @csrf()
+                        <button>Delete</button>
+                    </form>
+                    
+                    </td>
+                </tr>
+              
+            @endforeach
+        </tbody>
+    </table>
+
+</body>
+
+</html>
